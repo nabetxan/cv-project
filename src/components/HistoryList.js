3c450389fd0d1f36502cac4987f468b5474402b7
nabetxan <119922401+nabetxan@users.noreply.github.com>
2023-06-19 15:44:26 +0900

    fix: avoid repeating code

--- src/components/HistoryList.js
+++ src/components/HistoryList.js
@@ -1,5 +1,3 @@
-import { CATEGORY } from "./App";
-
 function HistoryList({
   formDataHistory,
   handleChangeHistory,
@@ -9,254 +7,80 @@ function HistoryList({
 }) {
   return (
     <>
-      {selectedButton === CATEGORY.GAKUREKI ? (
-        <>
-          <div className="border flex-grow"></div>
-          {/* input educational experience */}
-          <table className="">
-            <thead>
-              <tr>
-                <th className="width60px">年</th>
-                <th className="width60px">月</th>
-                <th>学歴</th>
-                <th className="width60px">
-                  <button
-                    onClick={() => {
-                      handleAddItemHistory(CATEGORY.GAKUREKI);
-                    }}
-                  >
-                    追加
-                  </button>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {formDataHistory.items.map(
-                (histroryItem) =>
-                  histroryItem.data &&
-                  histroryItem.category === CATEGORY.GAKUREKI &&
-                  histroryItem.data.map((data, index) => (
-                    <tr key={index}>
-                      <td>
-                        <input
-                          type="text"
-                          name="year"
-                          value={data.year}
-                          onChange={(e) =>
-                            handleChangeHistory(e, histroryItem.category, index)
-                          }
-                          className="text-center"
-                        />
-                      </td>
-                      <td>
-                        <input
-                          type="text"
-                          name="month"
-                          value={data.month}
-                          onChange={(e) =>
-                            handleChangeHistory(e, histroryItem.category, index)
-                          }
-                          className="text-center"
-                        />
-                      </td>
-                      <td>
-                        <input
-                          type="text"
-                          name="contents"
-                          value={data.contents}
-                          onChange={(e) =>
-                            handleChangeHistory(e, histroryItem.category, index)
-                          }
-                          className="flex-grow padding-side"
-                        />
-                      </td>
-                      <td>
-                        {
-                          <button
-                            onClick={() =>
-                              handleDeleteItemHistory(
-                                histroryItem.category,
-                                index
-                              )
-                            }
-                          >
-                            削除
-                          </button>
+      <div className="border flex-grow"></div>
+      <table className="">
+        <thead>
+          <tr>
+            <th className="width60px">年</th>
+            <th className="width60px">月</th>
+            <th>{selectedButton}</th>
+            <th className="width60px">
+              <button
+                onClick={() => {
+                  handleAddItemHistory(selectedButton);
+                }}
+              >
+                追加
+              </button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {formDataHistory.items.map(
+            (histroryItem) =>
+              histroryItem.data &&
+              histroryItem.category === selectedButton &&
+              histroryItem.data.map((data, index) => (
+                <tr key={index}>
+                  <td>
+                    <input
+                      type="text"
+                      name="year"
+                      value={data.year}
+                      onChange={(e) =>
+                        handleChangeHistory(e, histroryItem.category, index)
+                      }
+                      className="text-center"
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      name="month"
+                      value={data.month}
+                      onChange={(e) =>
+                        handleChangeHistory(e, histroryItem.category, index)
+                      }
+                      className="text-center"
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      name="contents"
+                      value={data.contents}
+                      onChange={(e) =>
+                        handleChangeHistory(e, histroryItem.category, index)
+                      }
+                      className="flex-grow padding-side"
+                    />
+                  </td>
+                  <td>
+                    {
+                      <button
+                        onClick={() =>
+                          handleDeleteItemHistory(histroryItem.category, index)
                         }
-                      </td>
-                    </tr>
-                  ))
-              )}
-            </tbody>
-          </table>
-        </>
-      ) : null}
-
-      {selectedButton === CATEGORY.SHOKUREKI ? (
-        <>
-          <div className="border flex-grow"></div>
-          {/* Table for Job Experience */}
-          <table className="">
-            <thead>
-              <tr>
-                <th className="width60px">年</th>
-                <th className="width60px">月</th>
-                <th>職歴</th>
-                <th className="width60px">
-                  <button
-                    onClick={() => {
-                      handleAddItemHistory(CATEGORY.SHOKUREKI);
-                    }}
-                  >
-                    追加
-                  </button>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {formDataHistory.items.map(
-                (histroryItem) =>
-                  histroryItem.data &&
-                  histroryItem.category === CATEGORY.SHOKUREKI &&
-                  histroryItem.data.map((data, index) => (
-                    <tr key={index}>
-                      <td>
-                        <input
-                          type="text"
-                          name="year"
-                          value={data.year}
-                          onChange={(e) =>
-                            handleChangeHistory(e, histroryItem.category, index)
-                          }
-                          className="text-center"
-                        />
-                      </td>
-                      <td>
-                        <input
-                          type="text"
-                          name="month"
-                          value={data.month}
-                          onChange={(e) =>
-                            handleChangeHistory(e, histroryItem.category, index)
-                          }
-                          className="text-center"
-                        />
-                      </td>
-                      <td>
-                        <input
-                          type="text"
-                          name="contents"
-                          value={data.contents}
-                          onChange={(e) =>
-                            handleChangeHistory(e, histroryItem.category, index)
-                          }
-                          className="flex-grow padding-side"
-                        />
-                      </td>
-                      <td>
-                        {
-                          <button
-                            onClick={() =>
-                              handleDeleteItemHistory(
-                                histroryItem.category,
-                                index
-                              )
-                            }
-                          >
-                            削除
-                          </button>
-                        }
-                      </td>
-                    </tr>
-                  ))
-              )}
-            </tbody>
-          </table>
-        </>
-      ) : null}
-
-      {selectedButton === CATEGORY.MENKYOSHIKAKU ? (
-        <>
-          <div className="border flex-grow"></div>
-          {/* input certificate */}
-          <table className="">
-            <thead>
-              <tr>
-                <th className="width60px">年</th>
-                <th className="width60px">月</th>
-                <th>免許・資格</th>
-                <th className="width60px">
-                  <button
-                    onClick={() => {
-                      handleAddItemHistory(CATEGORY.MENKYOSHIKAKU);
-                    }}
-                  >
-                    追加
-                  </button>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {formDataHistory.items.map(
-                (histroryItem) =>
-                  histroryItem.data &&
-                  histroryItem.category === CATEGORY.MENKYOSHIKAKU &&
-                  histroryItem.data.map((data, index) => (
-                    <tr key={index}>
-                      <td>
-                        <input
-                          type="text"
-                          name="year"
-                          value={data.year}
-                          onChange={(e) =>
-                            handleChangeHistory(e, histroryItem.category, index)
-                          }
-                          className="text-center"
-                        />
-                      </td>
-                      <td>
-                        <input
-                          type="text"
-                          name="month"
-                          value={data.month}
-                          onChange={(e) =>
-                            handleChangeHistory(e, histroryItem.category, index)
-                          }
-                          className="text-center"
-                        />
-                      </td>
-                      <td>
-                        <input
-                          type="text"
-                          name="contents"
-                          value={data.contents}
-                          onChange={(e) =>
-                            handleChangeHistory(e, histroryItem.category, index)
-                          }
-                          className="flex-grow padding-side"
-                        />
-                      </td>
-                      <td>
-                        {
-                          <button
-                            onClick={() =>
-                              handleDeleteItemHistory(
-                                histroryItem.category,
-                                index
-                              )
-                            }
-                          >
-                            削除
-                          </button>
-                        }
-                      </td>
-                    </tr>
-                  ))
-              )}
-            </tbody>
-          </table>
-        </>
-      ) : null}
+                      >
+                        削除
+                      </button>
+                    }
+                  </td>
+                </tr>
+              ))
+          )}
+        </tbody>
+      </table>
     </>
   );
 }
